afd01db4a396ba74e5ff051b7eb3c0896f4fc38e
Gavin Li <liyu@yahoo-inc.com>
2015-03-28 00:04:12 +0000

    add missing files for store callback and offset dedup

---
+++ src/main/java/com/yahoo/ads/pb/customization/DefaultStoreCallback.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2014 Yahoo! Inc. Licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or
+ * agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and
+ * limitations under the License. See accompanying LICENSE file.
+ */
+
+package com.yahoo.ads.pb.customization;
+import com.yahoo.ads.pb.util.ConfigurationManager;
+import com.yahoo.ads.pb.customization.CustomizationRegistry;
+import org.apache.curator.utils.CloseableUtils;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.cache.NodeCache;
+import org.apache.curator.framework.recipes.cache.NodeCacheListener;
+import org.apache.curator.framework.recipes.cache.ChildData;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.net.URLClassLoader;
+import java.net.URL;
+
+public class DefaultStoreCallback implements StoreCallback {
+    public boolean needCallback() {
+        return true;
+    }
+    public byte[] onStore(byte[] key, byte[] currentValue, byte[] toStore) {
+        return toStore;
+    }
+}
---
+++ src/main/java/com/yahoo/ads/pb/store/ValueOffset.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2014 Yahoo! Inc. Licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or
+ * agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and
+ * limitations under the License. See accompanying LICENSE file.
+ */
+
+package com.yahoo.ads.pb.store;
+
+public class ValueOffset {
+
+	public byte[] value;
+	public long offset;
+
+	public boolean equals(Object obj) {
+		if (this == obj) return true;
+		if (! (obj instanceof ValueOffset)) return false;
+
+		ValueOffset that = (ValueOffset)obj;
+
+		return (value == that.value) && (offset == that.offset);
+	}
+}
+
