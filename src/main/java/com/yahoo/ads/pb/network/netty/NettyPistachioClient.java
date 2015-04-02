0db7dbd2adbd233a7a24b2860ad67003b2f9b451
lvjq <lvjq@yahoo.com>
2015-04-02 14:31:25 -0700

    log

--- src/main/java/com/yahoo/ads/pb/network/netty/NettyPistachioClient.java
+++ src/main/java/com/yahoo/ads/pb/network/netty/NettyPistachioClient.java
@@ -188,7 +188,8 @@ public final class NettyPistachioClient implements PistachiosClientImpl{
     
     @Override
     public boolean delete(byte[] id) throws MasterNotFoundException, ConnectionBrokenException{
-		long partition = partitioner.getPartition(id, helixPartitionSpectator.getTotalPartition("PistachiosResource"));
+    	logger.info("delete call");
+    	long partition = partitioner.getPartition(id, helixPartitionSpectator.getTotalPartition("PistachiosResource"));
         if (isLocalCall(partition)) {
         	logger.info("local call");
            return PistachiosServer.handler.delete(id, partition);
