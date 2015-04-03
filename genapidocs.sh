6573ebcdfc4b7e77e722ebe0507cbcffdef65e57
Gavin Li <liyu@yahoo-inc.com>
2015-04-03 00:11:09 +0000

    add apidocs

--- genapidocs.sh
+++ genapidocs.sh
@@ -7,7 +7,7 @@ rm -rf /tmp/pistachio_apidocs
 mkdir /tmp/pistachio_apidocs
 cp -r ./target/site/apidocs /tmp/pistachio_apidocs/
 git checkout gh-pages
-cp -r /tmp/pistachio_apidocs/* ./apidocs/
+cp -r /tmp/pistachio_apidocs/apidocs/* ./apidocs/
 git commit -a -m "update apidocs"
 git push origin gh-pages
 git checkout master
